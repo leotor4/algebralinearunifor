fd73ae4e810cdbcb33ca590fb572641da6baa6ce
leotor4 <70244766+leotor4@users.noreply.github.com>
2020-09-01 22:40:15 -0300

    rafatoracao de funcoes
    Refatoracao de duas funcoes. E inicio do escopo daf funcao dot para ser implementada

--- linalg.js
+++ linalg.js
@@ -40,41 +40,103 @@ class LinearAlgebra{
 
     }
 
-    // operacao "multiplicacao escalar matriz"
-    // multiplica um valor escalar por todos os elementos da matriz
+    
     scalarmult(k, a)
     {
         let c = new Matrix(a.rows, a.cols);
 
-        for(let i = 1; i <= c.rows; i++)
-        {
-            for(let j = 1; j <= c.cols; j++)
-            {
-                c.set(i, j, a.get(i, j) * k);
-            }
-        }
+        
 
         return c;
     }
 
-    //opercao "multiplicacao elemento a elemento"
-    // multiplica os elementos de uma matriz a pelos elementos de uma matriz b
-    // num de linha e de coluna de matriz A deve ser igual a de B
+    
     times(a,b)
     {
+        // se for igual a number, eu faco a operacao escalar matriz
+        if(typeof a == "number")
+        {
 
-        if(a.rows != b.rows || a.cols != b.cols) throw "As matrizes são incompativeis."
+            if(typeof b != "object" || !(b instanceof Matrix))
+            {
+                throw "O parametro b deve ser uma matrix."
+            }
+            // operacao "multiplicacao escalar matriz"
+            // multiplica um valor escalar por todos os elementos da matriz
+            let c = new Matrix(a.rows, a.cols);
 
-        let c = new Matrix(a.rows, a.cols);
+            for(let i = 1; i <= c.rows; i++)
+            {
+                for(let j = 1; j <= c.cols; j++)
+                {
+                    c.set(i, j, a.get(i, j) * k);
+                }
+            }
+
+            //opercao "multiplicacao elemento a elemento"
+    // multiplica os elementos de uma matriz a pelos elementos de uma matriz b
+    // num de linha e de coluna de matriz A deve ser igual a de B
+        }else if(typeof a == "object" && a instanceof Matrix)
+        {
+            if(a.rows != b.rows || a.cols != b.cols) throw "As matrizes são incompativeis."
+
+            for(let i = 1; i <= c.rows; i++)
+            {
+                for(let j = 1; j <= c.cols; j++)
+                {
+                    c.set(i, j, a.get(i, j) * b.get(i, j));
+                }
+            }
+        }else
+        {
+            throw "O parametro a deve ser um escalar numerico ou uma matriz."
+        }
+
+        
+
+        return c;
+    }
+
+    // divisao elemento a elemento. Divide o elemento aij pelo elemento bij. Retorna uma matriz resultante.
+    //recebe uma matriz a e uma matriz b
+    div(a,b)
+    {
+        //verifica se a e b sao matrizes
+        if(typeof b != "object" || !(b instanceof Matrix) || typeof a != "object" || !(a instanceof Matrix))
+        {
+            throw "O parametro b deve ser uma matrix."
+        }
+
+
+        //verifica se as matrizes sao do mesmo tamanho
+        if(a.rows != b.rows || a.cols != b.cols) 
+            throw "As matrizes são incompativeis."
+        
+        //verifica se a matriz b possui algum elemento nulo
+        for(let i=0; i<b.values.length; i++)
+        {
+            if(b.values[i] == 0)
+            {
+                throw "A matriz b possui pelo menos um elemento nulo"
+            }
+        }
+
+        let c = new Matrix(a.cols, a.rows);
 
         for(let i = 1; i <= c.rows; i++)
         {
             for(let j = 1; j <= c.cols; j++)
             {
-                c.set(i, j, a.get(i, j) * b.get(i, j));
+                c.set(i, j, a.get(j, i) / b.get(i, j));
             }
         }
 
         return c;
+    }    
+
+    //funcao multiplicacao de matriz ou matriz x matriz
+    dot(a,b)
+    {
+
     }
 }
